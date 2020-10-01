394a274e314e4b14c2375ec004f052c49f391fc5
DevRaj <62476529+devrajash@users.noreply.github.com>
2020-10-02 00:13:41 +0530

    Update settings.py

--- task_03/settings.py
+++ task_03/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tnvhv(22@t1#1!f9$&6gl#0-&9(symuxb$=x_2h6zkg*tdi4iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
